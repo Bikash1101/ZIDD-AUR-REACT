2bb53df6710717f75bd6abcbee59a34b78733c85
Bikash <bikash1101@gmail.com>
2025-08-03 08:10:41 +0530

    commit counter app

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+      
+    const [counter , setCounter]= useState(15);
+
+
+      const addValue = ()=>{
+        if(counter < 20){
+            setCounter(counter + 1)
+        }
+      }
+
+      const removeValue = ()=>{
+        if(counter > 0){
+          setCounter(counter - 1)
+        }
+      }
+
+  return (
+    <>
+      
+      <h1>ZIDD aur REACT</h1>
+      <h2>Counter value : {counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>Decrease Value</button>
+    </>
+  )
+}
+
+export default App
